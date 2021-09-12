24d849258953445632d620c1d8a69a7b88f1fd4b
LehiChiang <tl502jlh214@gmail.com>
2021-09-12 12:32:15 +0800

    2021/09/11 每日一题

---
+++ src/leetcode/editor/cn/[21]合并两个有序链表.java
@@ -0,0 +1,61 @@
+package leetcode.editor.cn;
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+// 示例 1：
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+//
+// 示例 2：
+//输入：l1 = [], l2 = []
+//输出：[]
+//
+// 示例 3：
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+//
+// 提示：
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列
+// Related Topics 递归 链表 👍 1904 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import leetcode.editor.datastructure.ListNode;
+
+class mergeTwoListsSolution {
+
+//    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        if (l1 == null) {
+//            return l2;
+//        } else if (l2 == null) {
+//            return l1;
+//        } else if (l1.val < l2.val) {
+//            l1.next = mergeTwoLists(l1.next, l2);
+//            return l1;
+//        } else {
+//            l2.next = mergeTwoLists(l2.next, l1);
+//            return l2;
+//        }
+//    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode newHead = new ListNode();
+
+        ListNode pre = newHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = l1 == null ? l2 : l1;
+        return newHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
