8c6c41a0a7e8cab1c11eecb67705e64454daadcc
Lehi Chiang <tl502jlh214@gmail.com>
2021-09-15 17:50:16 +0800

    增加了Java知识点，修改了知识点代码模板

--- java/java核心技术卷一/Code.java
+++ java/java核心技术卷一/Code.java
@@ -1,8 +1,10 @@
 package java核心技术卷一;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 public class Code {
     public static void main(String[] args) {
-        String all = "java".repeat(5);
-        System.out.println(all);
+
     }
 }
