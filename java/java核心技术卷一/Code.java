3c37f54a4c0b31b182175f2c4ba80363ff1a2f16
Lehi Chiang <tl502jlh214@gmail.com>
2021-09-23 17:28:33 +0800

    java 核心技术

--- java/java核心技术卷一/Code.java
+++ java/java核心技术卷一/Code.java
@@ -2,6 +2,7 @@ package java核心技术卷一;
 
 import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Code {
     public static void main(String[] args) {
@@ -33,5 +34,18 @@ public class Code {
      public Employee() {
          name = "unknown";
          salary = 0;
+     }
+
+     @Override
+     public boolean equals(Object o) {
+         if (this == o) return true;
+         if (o == null || getClass() != o.getClass()) return false;
+         Employee employee = (Employee) o;
+         return id == employee.id && Double.compare(employee.salary, salary) == 0 && Objects.equals(name, employee.name);
+     }
+
+     @Override
+     public int hashCode() {
+         return Objects.hash(id, name, salary);
      }
  }
