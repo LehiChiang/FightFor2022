3b3858f6d15588b19ac758ff40c65afbd10e53fe
Lehi Chiang <tl502jlh214@gmail.com>
2021-10-23 17:14:11 +0800

    Java集合 力扣刷题

--- java/java核心技术卷一/chap9/code_9_3_3.java
+++ java/java核心技术卷一/chap9/code_9_3_3.java
@@ -4,12 +4,12 @@ import java.util.*;
 
 public class code_9_3_3 {
     public static void main(String[] args) {
-        HashSet<Integer> set = new HashSet<>();
-        set.add(1);
-        set.add(1);
-        set.add(2);
+        Set<String> set = new TreeSet<>();
+        set.add("Jiang");
+        set.add("Jiang");
+        set.add("Jiang");
+        set.add("Liu");
+        set.add("Annie");
         System.out.println(set);
-        Deque<Integer> deque1 = new ArrayDeque<>();
-        List<Integer> deque2 = new LinkedList<>();
     }
 }
