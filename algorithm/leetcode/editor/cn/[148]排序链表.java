c656144ceb7b68b3c04c4f7b59dff33154ed0ef4
Lehi Chiang <tl502jlh214@gmail.com>
2021-11-04 23:02:05 +0800

    codetop练习题

---
+++ algorithm/leetcode/editor/cn/[148]排序链表.java
@@ -0,0 +1,41 @@
+package leetcode.editor.cn;
+
+//给你链表的头结点 head ，请将其按升序排列并返回排序后的链表 。
+//
+// 进阶：
+// 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+// 示例 1：
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+// 示例 2：
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+// 示例 3：
+//输入：head = []
+//输出：[]
+// 提示：
+// 链表中节点的数目在范围 [0, 5 * 10⁴] 内 
+// -10⁵ <= Node.val <= 10⁵
+// Related Topics 链表 双指针 分治 排序 归并排序 👍 1356 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import datastructure.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class sortListSolution {
+    public ListNode sortList(ListNode head) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
